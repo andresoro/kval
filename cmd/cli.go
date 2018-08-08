f9a7c0b715371f7a0cfd54631550157dc5fec6c0
Andres Orozco <ao15@my.fsu.edu>
2018-08-07 21:00:11 -0400

    add shell for prompt

--- cmd/cli.go
+++ cmd/cli.go
@@ -1,5 +1,41 @@
 package cmd
 
-func init() {
+import (
+	"github.com/c-bata/go-prompt"
+	"github.com/spf13/cobra"
+)
 
+var cliCmd = &cobra.Command{
+	Use:   "cli",
+	Short: "Cli for kval server",
+	Long:  "Start a prompt to interact with kval server",
+	Run: func(cmd *cobra.Command, args []string) {
+		run()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(cliCmd)
+}
+
+func run() {
+	p := prompt.New(
+		execute,
+		completer,
+		prompt.OptionPrefix(">>> "),
+		prompt.OptionTitle("kval"),
+	)
+	p.Run()
+}
+
+func execute(input string) {
+
+}
+
+func completer(t prompt.Document) []prompt.Suggest {
+	return []prompt.Suggest{
+		{Text: "add"},
+		{Text: "get"},
+		{Text: "delete"},
+	}
 }
